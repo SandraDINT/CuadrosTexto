a08223f2d58ee4ab7a8b5c93a3e44fca02832815
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2021-10-04 14:26:08 +0200

    Agregar archivos de proyecto.

---
+++ CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuadrosTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            nombreTextBox.Tag = textBlockAyudaNombre;
+            apellidoTextBox.Tag = textBlockAyudaApellido;
+        }
+
+        private void NombreYApellidoTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            TextBlock textBlock = (TextBlock)textBox.Tag;
+            if (e.Key == Key.F1 && textBlock.Visibility != Visibility.Visible)
+                textBlock.Visibility = Visibility.Visible;
+            else if (e.Key == Key.F1 && textBlock.Visibility == Visibility.Visible)
+                textBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void Edad_KeyUp(object sender, KeyEventArgs e)
+        {
+            bool seHaPodidoParsear = Int32.TryParse(textBoxEdad.Text, out _);
+            if (!seHaPodidoParsear)
+                MessageBox.Show("¡No es un entero!");
+        }
+    }
+}
